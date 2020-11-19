727263e81ead007f5a5df0c26e61337518ca7c2a
Reina Uhler <reinadu@gmail.comgit>
2020-11-19 13:02:16 -0800

    state redux cards

--- src/components/pages/ReviewAnswer.js
+++ src/components/pages/ReviewAnswer.js
@@ -2,42 +2,68 @@ import React from "react";
 import thumbsUpIcon from "../../icons/thumbs-up.svg";
 import AppTemplate from "../ui/AppTemplate";
 import { Link } from "react-router-dom";
-import memoryCards from "../../mock-data/memory-cards";
-const memoryCard = memoryCards[2];
+import axios from "axios";
+import { connect } from "react-redux";
+import actions from "../../store/actions";
 
-export default function ReviewAnswer() {
-   return (
-      <AppTemplate>
-         <div className="mb-5">
-            <div className="card">
-               <div className="card-body bg-primary lead">
-                  {memoryCard.imagery}
+class ReviewAnswer extends React.Component {
+   goToNextCard() {
+      this.props.dispatch({ type: actions.UPDATE_INDEX_OF_CURRENT_CARD });
+      this.props.history.push("/review-imagery");
+   }
+
+   render() {
+      const memoryCard = this.props.queuedCards[this.props.indexOfCurrentCard];
+      return (
+         <AppTemplate>
+            <div className="mb-5">
+               <div className="card">
+                  <div className="card-body bg-primary lead">
+                     {memoryCard && memoryCard.imagery}
+                  </div>
+               </div>
+               <div className="card">
+                  <div className="card-body bg-secondary lead">
+                     {memoryCard && memoryCard.answer}
+                  </div>
                </div>
             </div>
-            <div className="card">
-               <div className="card-body bg-secondary lead">
-                  {memoryCard.answer}
-               </div>
-            </div>
-         </div>
 
-         <Link to="/edit" className="btn btn-link">
-            Edit
-         </Link>
-         <div className="float-right">
-            <Link to="/review-empty" className="btn btn-outline-primary">
-               Needs work
+            <Link to="/edit" className="btn btn-link">
+               Edit
             </Link>
-            <Link to="/review-empty" className="btn btn-primary ml-4">
-               <img
-                  src={thumbsUpIcon}
-                  width="20px"
-                  style={{ marginBottom: "5px", marginRight: "4px" }}
-                  alt=""
-               />
-               Got it
-            </Link>
-         </div>
-      </AppTemplate>
-   );
+            <div className="float-right">
+               <button
+                  className="btn btn-outline-primary"
+                  onClick={() => {
+                     this.goToNextCard();
+                  }}
+               >
+                  Needs work
+               </button>
+               <button
+                  className="btn btn-primary ml-4"
+                  onClick={() => {
+                     this.goToNextCard();
+                  }}
+               >
+                  <img
+                     src={thumbsUpIcon}
+                     width="20px"
+                     style={{ marginBottom: "5px", marginRight: "4px" }}
+                     alt=""
+                  />
+                  Got it
+               </button>
+            </div>
+         </AppTemplate>
+      );
+   }
 }
+function mapStateToProps(state) {
+   return {
+      queuedCards: state.queuedCards,
+      indexOfCurrentCard: state.indexOfCurrentCard,
+   };
+}
+export default connect(mapStateToProps)(ReviewAnswer);
--- src/store/reducers/indexOfCurrentCard.js
+++ src/store/reducers/indexOfCurrentCard.js
@@ -3,7 +3,7 @@ import actions from "../actions";
 export default function indexOfCurrentCard(state = 0, action) {
    switch (action.type) {
       case actions.UPDATE_INDEX_OF_CURRENT_CARD:
-         return action.payload;
+         return state + 1;
       default:
          return state;
    }
